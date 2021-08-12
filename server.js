93ce343a5df10b5c41b13c6f51e8a3331e2f4eb3
giancode1 <gieg5c@gmail.com>
2021-08-11 20:03:51 -0500

    middleware

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const morgan = require("morgan"); //un middleware
 const app = express();
 
-app.use(express.json());   //ahora entiende json cuando un cliente lo envie
-app.use(express.urlencoded({extend: false}));
+//middleware, manejador de peticion, lapuedo ejecutar antes que llegue a su ruta original
+/* function logger(req, res, next) {
+  console.log(`Peticion received: ${req.protocol}://${req.get('host')}${req.originalUrl}`);
+  next();
+} */
+
+
+app.use(express.json()); //ahora entiende json cuando un cliente lo envie
+//app.use(logger);
+app.use(morgan('dev')); 
+app.use(express.urlencoded({ extend: false }));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -18,15 +28,15 @@ app.get("/test", function (req, res) {
 });
 
 app.get("/user", function (req, res) {
-    res.json({
-        "username": "Gian",
-        "country": "Ecuador"
-    });
+  res.json({
+    username: "Gian",
+    country: "Ecuador",
   });
+});
 
 app.post("/user/:id", function (req, res) {
-    console.log("req.body",req.body) //cuerpo de la peticion
-    console.log("req.params:",req.params)  //   user/todoesto
+  console.log("req.body", req.body); //cuerpo de la peticion
+  console.log("req.params:", req.params); //   user/todoesto
   res.send("Got a POST request");
 });
 

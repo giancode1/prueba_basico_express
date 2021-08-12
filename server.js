d1519be5a068e92fa05cbac4160e3c43a1e8b135
giancode1 <gieg5c@gmail.com>
2021-08-11 20:52:48 -0500

    Static

--- server.js
+++ server.js
@@ -1,19 +1,11 @@
 const express = require("express");
-const morgan = require("morgan"); //un middleware
+const morgan = require("morgan");
 const app = express();
 
-//middleware, manejador de peticion, lapuedo ejecutar antes que llegue a su ruta original
-/* function logger(req, res, next) {
-  console.log(`Peticion received: ${req.protocol}://${req.get('host')}${req.originalUrl}`);
-  next();
-} */
-
-
-app.use(express.json()); //ahora entiende json cuando un cliente lo envie
-//app.use(logger);
-app.use(morgan('dev')); 
+app.use(express.json());
+app.use(morgan("dev"));
 app.use(express.urlencoded({ extend: false }));
-
+app.use(express.static("public")); // se publica en la raiz
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -35,8 +27,8 @@ app.get("/user", function (req, res) {
 });
 
 app.post("/user/:id", function (req, res) {
-  console.log("req.body", req.body); //cuerpo de la peticion
-  console.log("req.params:", req.params); //   user/todoesto
+  console.log("req.body", req.body);
+  console.log("req.params:", req.params);
   res.send("Got a POST request");
 });
 
@@ -47,9 +39,3 @@ app.put("/", function (req, res) {
 app.delete("/", function (req, res) {
   res.send("Got a DELETE request");
 });
-
-/* app.all('/user',(req, res, next) => {
-    console.log('Por aqui paso')
-    //res.send('Terminado')
-    next()
-}) */
